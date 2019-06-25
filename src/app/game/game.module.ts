d1bb0783cb94b0e87b5b2a7f8000f1c036ef5874
Andres Rubio del Rio <arubio.formacion.tic@gmail.com>
2019-06-25 18:59:42 +0200

    Fase 1: Creación del componente game perteneciente al módulo game

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [GameComponent],
   imports: [
     CommonModule
   ]
